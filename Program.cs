41154073423a8f34eb17aca6b3074a01c2b3a40b
rain <35134037+xrainx@users.noreply.github.com>
2018-05-15 10:57:30 +0200

    files (template)

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Bot.Examples.Echo
+{
+    public static class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("TOKEN");
+
+        public static void Main(string[] args)
+        {
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.Text) return;
+
+            switch (message.Text.Split(' ').First())
+            {
+                // show help commands
+                case "/help":
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "This bot is still under development.");
+                    break;
+                // new user?
+                case "/start":
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Welcome!");
+                    break;
+
+                default:
+                    const string usage = @"
+Welcome to VATSIM Bot v1.0! Type /help to get started.";
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        usage,
+                        replyMarkup: new ReplyKeyboardRemove());
+                    break;
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQuery.Id,
+                $"Received {callbackQuery.Data}");
+
+            await Bot.SendTextMessageAsync(
+                callbackQuery.Message.Chat.Id,
+                $"Received {callbackQuery.Data}");
+        }
+
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            Console.WriteLine($"Received inline query from: {inlineQueryEventArgs.InlineQuery.From.Id}");
+
+            InlineQueryResultBase[] results = {
+                new InlineQueryResultLocation(
+                    id: "1",
+                    latitude: 40.7058316f,
+                    longitude: -74.2581888f,
+                    title: "New York")   // displayed result
+                    {
+                        InputMessageContent = new InputLocationMessageContent(
+                            latitude: 40.7058316f,
+                            longitude: -74.2581888f)    // message if result is selected
+                    },
+
+                new InlineQueryResultLocation(
+                    id: "2",
+                    latitude: 13.1449577f,
+                    longitude: 52.507629f,
+                    title: "Berlin") // displayed result
+                    {
+
+                        InputMessageContent = new InputLocationMessageContent(
+                            latitude: 13.1449577f,
+                            longitude: 52.507629f)   // message if result is selected
+                    }
+            };
+
+            await Bot.AnswerInlineQueryAsync(
+                inlineQueryEventArgs.InlineQuery.Id,
+                results,
+                isPersonal: true,
+                cacheTime: 0);
+        }
+
+        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine($"Received inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+        }
+    }
+}
